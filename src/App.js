72f447efbe2846e49bd678c6555a9abd0bbf11de
Ali Raza <80849102+Ali-Raza-400@users.noreply.github.com>
2023-05-27 23:27:20 +0500

    register user using firebase and store information in firebase cloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Offer from './pages/Offer'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Explore/>} />
+          <Route path='/offers' element={<Offer/>} />
+          <Route path='/profile' element={<SignIn/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
