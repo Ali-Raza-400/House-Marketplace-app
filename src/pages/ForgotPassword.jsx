cdc3f1cebf7f237110bcacbccd53281f308e0fda
Ali Raza <80849102+Ali-Raza-400@users.noreply.github.com>
2023-05-28 20:49:10 +0500

    add reset password functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const auth = getAuth();
+    try {
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email Send", {
+        position: toast.POSITION.TOP_LEFT,
+      });
+    } catch (error) {
+      toast.error("Not Sent", {
+        position: toast.POSITION.TOP_LEFT,
+      });
+    }
+  };
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <div>
+      <form onSubmit={onSubmit}>
+        <input placeholder="Enter Email" onChange={onChange} value={email} />
+        <button type="submit">Sent Link</button>
+      </form>
+    </div>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
